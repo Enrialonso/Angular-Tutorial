f983791ab3c373f833095a179744324272c28b16
Enrique Alonso <ealonso@full-on-net.com>
2017-11-15 21:59:24 +0100

    Agregado ejemplos de directiva ngFor

--- src/app/directiva-estructura-ng-for/directiva-estructura-ng-for.component.ts
+++ src/app/directiva-estructura-ng-for/directiva-estructura-ng-for.component.ts
@@ -10,6 +10,19 @@ export class DirectivaEstructuraNgForComponent implements OnInit {
 
   textoDirectivaNgFor = '*ngFor="let objeto/propiedad of objetos/propiedades"'
 
+  textoObjetoNgFor0 = 'Objeto: "Tienda" :' 
+  textoObjetoNgFor1 = '[ {item: "Disco duro", marca: "Maxtor", cantidad: "40", precio: "50"},'
+  textoObjetoNgFor2 =  '{item: "Unidad DVD", marca: "LG", cantidad: "12", precio: "42"},'
+  textoObjetoNgFor3 =  '{item: "Mother Board", marca: "Asus", cantidad: "30", precio: "87"},'
+  textoObjetoNgFor4 =  '{item: "Case", marca: "Thermaltake", cantidad: "5", precio: "60"},'
+  textoObjetoNgFor5 =  '{item: "SSD", marca: "Intel", cantidad: "10", precio: "112"} ]'
+                
+  ObjetoNgFor = [ {item: "Disco duro", marca: "Maxtor", cantidad: 40, precio: 50},
+                  {item: "Unidad DVD", marca: "LG", cantidad: 12, precio: 42},
+                  {item: "Mother Board", marca: "Asus", cantidad: 30, precio: 87},
+                  {item: "Case", marca: "Thermaltake", cantidad: 5, precio: 60},
+                  {item: "SSD", marca: "Intel", cantidad: 10, precio: 112} ]
+
   constructor() { } 
 
   ngOnInit() {
